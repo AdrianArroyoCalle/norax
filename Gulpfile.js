2054aa466b7fbeb99e20a16cdb49313c066e1316
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2016-01-15 21:31:50 +0100

    Redesign front page using Foundation

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,7 @@
 var gulp=require("gulp");
 var ejs=require("ejs");
 var rimraf=require("rimraf");
-var shst=require("sh.st")();
+var shst=require("adf.ly")();
 var fs=require("fs");
 var mkdirp=require("mkdirp");
 var ghPages = require('gulp-gh-pages');
@@ -16,12 +16,19 @@ var langPack=require("./lang.json");
 /* Route to unique page (merge Changelog and Welcome) */
 /* Some common translations */
 /* Share buttons */
+/* Migrate all addons */
+/* Translations */
 
 /* /firefox-addons/page/google-share/index.html or es.html, fr.html */
 /* Travis */
 /* Search Engine */
 /* PageSpeed, W3C */
 /* Meta and Link tags */
+/* Amazon */
+/* MailChimp */
+/* Donate */
+/* Contact */
+/* Página 404 */
 
 gulp.task("clean",function(cb){
 	rimraf("_site",cb);
@@ -37,10 +44,23 @@ gulp.task("img",function(){
 		.pipe(gulp.dest("_site/img/"));
 });
 
+gulp.task("foundation",function(){
+	return gulp.src("css/foundation.min.css")
+		.pipe(gulp.dest("_site/"));
+});
+
 gulp.task("css",function(cb){
 	var cssFile=fs.readFileSync("css/main.less","utf-8");
-	var ejsCss=ejs.render(cssFile,{addons: addons});
+	var copyAddons=Object.create(addons);
+	copyAddons["adsense"]={};
+	copyAddons["adsense"].color="#E56B20";
+	copyAddons["pick"]={};
+	copyAddons["pick"].color="#A4212F";
+	copyAddons["share"]={};
+	copyAddons["share"].color="#5FEC58";
+	var ejsCss=ejs.render(cssFile,{addons: copyAddons});
 	less.render(ejsCss,function(e,output){
+		mkdirp.sync("_site");
 		fs.writeFileSync("_site/main.min.css",output.css);
 		cb();
 	});
@@ -49,24 +69,23 @@ gulp.task("css",function(cb){
 gulp.task("addonPage",function(cb){
 	for(var id in addons){
 		var addonPage=fs.readFileSync("ejs/addonPage.ejs","utf-8");
-		shst.short(addons[id].download,function(url){
-			for(var lang in addons[id].description){				
-				var html=ejs.render(addonPage,{
-					lang: langPack[lang],
-					addon: addons[id],
-					imgPrefix: "../../../img/",
-					downloadLink: url
-				});
-				mkdirp.sync("_site/"+lang+"/addon/"+id);
-				fs.writeFileSync("_site/"+lang+"/addon/"+id+"/index.html",html);
-			}
-		});
+		for(var lang in addons[id].description){				
+			var html=ejs.render(addonPage,{
+				lang: langPack[lang],
+				addon: addons[id],
+				imgPrefix: "../../../img/",
+				downloadLink: addons[id].download
+			});
+			mkdirp.sync("_site/"+lang+"/addon/"+id);
+			fs.writeFileSync("_site/"+lang+"/addon/"+id+"/index.html",html);
+		}
 	}
 	cb();
 });
 
 gulp.task("indexPage",function(){
 	var indexPage=fs.readFileSync("ejs/index.ejs","utf-8");
+	
 	for(var lang in langPack){
 		if(lang==="en"){
 			var html=ejs.render(indexPage,{
@@ -88,6 +107,6 @@ gulp.task("indexPage",function(){
 	}
 });
 
-gulp.task("default",["img","css","indexPage","addonPage"],function(){
+gulp.task("default",["foundation","img","css","indexPage","addonPage"],function(){
 	
 });
